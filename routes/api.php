853a7d4f7d0bfa248c5dae76059866e9d2798ed6
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-07-07 18:54:08 -0500

    Correciones porque el profe me mandó a recu :'v

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ Route::group(['prefix' => 'v1/pizzas', 'middleware'=>['cors']], function(){
 	Route::get('/menu','MenuController@index');
 	Route::get('/menu/especialidad/{id}', 'MenuController@getDetails');
 	Route::get('/order/create', 'PizzasController@create');
-	Route::get('/cobrar/{token}', 'PagoControlller@cobrar');
+	Route::get('/cobrar/{token}/{pago}', 'PagoControlller@cobrar');
 	Route::get('/ordenes', 'PizzasController@getAll');
 });
