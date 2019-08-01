941b18fd1a68f1c83fc50782fbd235fba3f89f3b
David Pérez S <playeando.97@gmail.com>
2019-08-01 10:02:41 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1', 'middleware'=>['cors']], function(){
 
+	// Ruta para los pagos
+
 	Route::get('/cobrar/{token}/{pago}', 'PagoControlller@cobrar');
 	
 });
