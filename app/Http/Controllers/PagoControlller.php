3abe881d1a1be3039d04b8a9e632698860a32e57
David Pérez S <playeando.97@gmail.com>
2019-08-01 02:38:10 -0500

    Caambios menores
    Cambios en rutas y en PagoController.

--- app/Http/Controllers/PagoControlller.php
+++ app/Http/Controllers/PagoControlller.php
@@ -13,23 +13,30 @@ class PagoControlller extends Controller
     public static function cobrar($token_compra, $monto)
     {
         // dd($token_compra);
-        Stripe::setApiKey(config('services.stripe.secret'));
-        $customer = Customer::create(array(
-            'email' => 'test@ids.com',
-            'source'  => $token_compra
-        ));
 
-        $charge = Charge::create(array(
-            'customer' => $customer->id,
-            'amount'   => round($monto, 2),
-            'currency' => 'MXN'
-        ));
-
-        return response()->json(
-            [
-                'OK'
-            ]
-            ,200
-        );
+        try {
+            Stripe::setApiKey(config('services.stripe.secret'));
+            $customer = Customer::create(array(
+                'email' => 'test@ids.com',
+                'source'  => $token_compra
+            ));
+    
+            $charge = Charge::create(array(
+                'customer' => $customer->id,
+                'amount'   => round($monto, 2),
+                'currency' => 'MXN'
+            ));
+    
+            return response()->json(
+                [
+                    'OK'
+                ]
+                ,200
+            );
+        } catch (Exception $e) {
+            return response()->json([
+                $e->getMessage()
+            ], 200);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 
-Route::group(['prefix' => 'v1/pizzas', 'middleware'=>['cors']], function(){
+Route::group(['prefix' => 'v1', 'middleware'=>['cors']], function(){
 	Route::get('/menu','MenuController@index');
 	Route::get('/menu/especialidad/{id}', 'MenuController@getDetails');
 	Route::get('/order/create', 'PizzasController@create');
 	Route::get('/cobrar/{token}/{pago}', 'PagoControlller@cobrar');
 	Route::get('/ordenes', 'PizzasController@getAll');
+	
 });
