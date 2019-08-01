b95c0335d83457613ddc6b02d76730e22e90cd37
David Pérez S <playeando.97@gmail.com>
2019-08-01 02:56:42 -0500

    Se removieron archivos innecesarios para el Deploy

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,7 @@
 use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1', 'middleware'=>['cors']], function(){
-	Route::get('/menu','MenuController@index');
-	Route::get('/menu/especialidad/{id}', 'MenuController@getDetails');
-	Route::get('/order/create', 'PizzasController@create');
+
 	Route::get('/cobrar/{token}/{pago}', 'PagoControlller@cobrar');
-	Route::get('/ordenes', 'PizzasController@getAll');
 	
 });
